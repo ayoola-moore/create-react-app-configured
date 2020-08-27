2688f33b6a688e4801332f053f859d43999decf0
hayoola <onaolapo.ayo@gmail.com>
2020-08-27 23:54:35 +0400

    remove thrown error

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,8 @@ import React from 'react'
 import './App.css'
 
 function App() {
-    function throwError() {
-        throw new Error('Oh no!')
-    }
     return (
         <div className="App">
-            {throwError()}
             <h1> start writing code because you rock !! </h1>
         </div>
     )
